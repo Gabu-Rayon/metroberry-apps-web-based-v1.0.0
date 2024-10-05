933712d7e66f24c44ce5dce41c669e1cbacad824
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-10-05 13:29:47 +0300

    commit the change made on customer Auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasPermissions;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles, HasPermissions, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +26,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'address',
+        'avatar',
+        'created_by',
+        'role'
     ];
 
     /**
@@ -28,9 +39,15 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
+        'created_at',
+        'updated_at',
         'password',
         'remember_token',
+        'roles',
+        'permissions',
     ];
+
+    // protected $with = ['organisation'];
 
     /**
      * Get the attributes that should be cast.
@@ -41,7 +58,56 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'phone_verified_at' => 'datetime',
+            'phone_status' => 'boolean',
+            'email_status' => 'boolean',
+            'organisation_status' => 'boolean',
             'password' => 'hashed',
         ];
+    }
+
+    public function driver()
+    {
+        return $this->hasOne(Driver::class);
+    }
+
+    public function customer(): HasOne
+    {
+        return $this->hasOne(Customer::class, 'user_id', 'id');
+    }
+    public function organisation()
+    {
+        return $this->hasOne(Organisation::class);
+    }
+
+
+    // public function organisation(): BelongsTo
+    // {
+    //     return $this->belongsTo(Organisation::class);
+    // }
+
+    
+    public function getOrganisationAttribute()
+    {
+        return $this->organisationRelation;
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::deleting(function ($user) {
+            $user->driver()->delete();
+            $user->customer()->delete(); 
+            $user->organisation()->delete();
+        });
+    }
+    public function createdVehicleServices()
+    {
+        return $this->hasMany(VehicleService::class, 'created_by');
+    }
+
+    public function fuelling_station()
+    {
+        return $this->hasOne(RefuellingStation::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// All Routes
+require __DIR__ . '/auth.php';
+require __DIR__ . '/customer.php';
+require __DIR__ . '/driver.php';
